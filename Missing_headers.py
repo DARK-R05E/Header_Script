59d8f3a42ca14bbb6cb10c2faf137427dfc5dde2
DARK_R05E <91992401+DARK-R05E@users.noreply.github.com>
2024-07-22 15:03:18 +0100

    Add files via upload

--- Missing_headers.py
+++ Missing_headers.py
@@ -3,21 +3,34 @@ import sys
 from termcolor import colored
 import warnings
 
-warnings.filterwarnings('ignore') 
-url = sys.argv[1] 
+warnings.filterwarnings('ignore')
+
+url = sys.argv[1]
+
+print('\n\nTarget URI is: \033[0;36m%s'%url)
+print(colored("",'white'),end='')
 
 try:
     g = requests.get(url)
+    print('\n')
 except:
-    print('ssl error but we still check:)')
+    print('\nAn ',end='')
+    print(colored('SSL/TLS error','red'),end='')
+    print(' has occured. Please investigate.\n')
     g = requests.get(url, verify=False)
-	
 website = g.headers
 
-options = ["X-Frame-Options", "Content-Security-Policy", "Strict-Transport-Security", "X-XSS-Protection", "X-Content-Type-Options"]
+options = ["X-Frame-Options", "Content-Security-Policy", "Strict-Transport-Security", "X-Content-Type-Options","X-XSS-Protection"]
 for new in options:
-    if new in website:
-        print(colored('%s is present.\n'%new, 'green'))
+    if new == "X-XSS-Protection":
+        print(colored('%s is present, and has been deprecated.'%new, 'red'))
+        print("Its value is: %s\n"%website[new])
+    elif new in website:
+        print(colored('%s is present.'%new, 'green'))
+        print("Its value is: %s\n"%website[new])
+    
+
     else:
         print(colored('%s is not present.\n'%new, 'red'))
+
 
